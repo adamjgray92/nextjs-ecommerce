e29a96cb7f90ae8d2d9978cc2e11a350be1ab743
Adam <adamjamesgray.1992@gmail.com>
2023-07-02 14:14:53 +0100

    Add root folder and installed ui/button

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <Button size='default' variant='ghost'>
+        Click me
+      </Button>
+    </div>
+  );
+}
