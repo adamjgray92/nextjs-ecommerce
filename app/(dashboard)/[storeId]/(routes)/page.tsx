a673eb574f68b99eea27a66aa30663bf8ac8ef48
Adam <adamjamesgray.1992@gmail.com>
2023-07-09 20:30:04 +0100

    Add navbar and store switcher

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,15 @@
-export default function DashboardPage() {
-  return <div>This is a dashboard</div>;
+import prismadb from '@/lib/prismadb';
+import React from 'react';
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+export default async function DashboardPage({
+  params,
+}: React.PropsWithChildren<DashboardPageProps>) {
+  const store = await prismadb.store.findFirst({
+    where: { id: params.storeId },
+  });
+  return <div>Active Store: {store?.name}</div>;
 }
