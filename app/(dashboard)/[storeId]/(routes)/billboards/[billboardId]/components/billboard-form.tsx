e524f251059cbcebbf5afee8187910c02a9415e7
Adam <adamjamesgray.1992@gmail.com>
2023-07-15 19:48:55 +0100

    BillboardsPage component and form

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -0,0 +1,143 @@
+'use client';
+
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Trash2Icon } from 'lucide-react';
+import { Billboard } from '@prisma/client';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useParams, useRouter } from 'next/navigation';
+import { toast } from 'react-hot-toast';
+import axios from 'axios';
+
+import { Button } from '@/components/ui/button';
+import { Heading } from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import AlertModal from '@/components/modals/alert-modal';
+import ApiAlert from '@/components/ui/api-alert';
+import useOrigin from '@/hooks/use-origin';
+
+interface BillboardFormProps {
+  initialData: Billboard | null;
+}
+
+const formSchema = z.object({
+  label: z.string().min(1),
+  imageUrl: z.string().min(1),
+});
+
+type BillboardFormValues = z.infer<typeof formSchema>;
+
+export default function BillboardForm({ initialData }: BillboardFormProps) {
+  const params = useParams();
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const origin = useOrigin();
+
+  const title = initialData ? 'Edit billboard' : 'Create billboard';
+  const description = initialData
+    ? 'Edit a billboard'
+    : 'Create a new Billboard';
+  const toastMessage = initialData
+    ? 'Billboard updated.'
+    : 'Billboard created.';
+  const action = initialData ? 'Save changes' : 'Create';
+
+  const form = useForm<BillboardFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || { label: '', imageUrl: '' },
+  });
+
+  const onSubmit = async (data: BillboardFormValues) => {
+    try {
+      setIsLoading(true);
+
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success('Store updated.');
+    } catch (error) {
+      toast.error('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push('/');
+      toast.success('Store deleted.');
+    } catch (error) {
+      toast.error('Make sure you remove all products and categories first.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        isLoading={isLoading}
+      />
+      <div className='flex items-center justify-between'>
+        <Heading title={title} description={description} />
+        {initialData ? (
+          <Button
+            disabled={isLoading}
+            variant='destructive'
+            size='icon'
+            onClick={() => setOpen(true)}
+          >
+            <Trash2Icon className='h-4 w-4' />
+          </Button>
+        ) : null}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='space-y-8 w-full'
+        >
+          <div className='grid grid-cols-3 gap-8'>
+            <FormField
+              control={form.control}
+              name='label'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Label:</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder='Billboard label'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={isLoading} className='ml-auto' type='submit'>
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+}
