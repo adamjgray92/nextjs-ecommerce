1b69c40d1f950b01351df659f50bd72072c327b2
Adam <adamjamesgray.1992@gmail.com>
2023-07-20 22:53:23 +0100

    Add data table

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,28 @@
-import BillboardClient from './components/billboard-client';
+import { format } from 'date-fns';
 
-export default function BillboardsPage() {
+import prismadb from '@/lib/prismadb';
+import BillboardClient from './components/billboard-client';
+import { BillboardColumn } from './components/columns';
+
+export default async function BillboardsPage({
+  params,
+}: {
+  params: { storeId: string };
+}) {
+  const billboards = await prismadb.billboard.findMany({
+    where: { storeId: params.storeId },
+  });
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }));
+
   return (
     <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
-        <BillboardClient />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   );
