2797907e64171ea0f997dee159480d98a1e1bd19
Adam <adamjamesgray.1992@gmail.com>
2023-11-08 23:51:37 +0000

    Add API alerts for Billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-client.tsx
@@ -8,6 +8,7 @@ import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 import { BillboardColumn, columns } from './columns';
 import { DataTable } from '@/components/ui/data-table';
+import ApiList from '@/components/ui/api-list';
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -33,6 +34,9 @@ export default function BillboardClient({ data }: BillboardClientProps) {
       </div>
       <Separator />
       <DataTable searchKey='label' columns={columns} data={data} />
+      <Heading title='API' description='API calls for Billboards' />
+      <Separator />
+      <ApiList entityName='billboards' entityIdName='billboardId' />
     </>
   );
 }
