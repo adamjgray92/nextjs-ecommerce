140ae4868b1241379a27f2c236be375d864ec9a5
Adam <adamjamesgray.1992@gmail.com>
2023-12-03 20:10:41 +0000

    Add category pages

---
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,33 @@
+import { format } from 'date-fns';
+
+import prismadb from '@/lib/prismadb';
+import { CategoryColumn } from './components/columns';
+import CategoryClient from './components/category-client';
+
+export default async function CategoriesPage({
+  params,
+}: {
+  params: { storeId: string };
+}) {
+  const categories = await prismadb.category.findMany({
+    where: { storeId: params.storeId },
+    include: {
+      billboard: true,
+    },
+  });
+
+  const formattedCategories: CategoryColumn[] = categories.map((item) => ({
+    id: item.id,
+    name: item.name,
+    billboardLabel: item.billboard.label,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }));
+
+  return (
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        <CategoryClient data={formattedCategories} />
+      </div>
+    </div>
+  );
+}
